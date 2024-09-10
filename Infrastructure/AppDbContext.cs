82c14d2acf09ad526115fb33e3a3fb98a5fc9303
MinhQuan0716 <minhquantrantrinh731@gmail.com>
2024-09-10 20:17:07 +0700

    Init project

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Infrastructure
+{
+    public static  class DependencyInjection
+    {
+        public static IServiceCollection AddInfratructureService(this IServiceCollection services, string databaseConnection)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(databaseConnection).EnableSensitiveDataLogging());
+            return services;
+        }
+    }
+}
