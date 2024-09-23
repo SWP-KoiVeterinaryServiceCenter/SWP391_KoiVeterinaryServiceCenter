932519f87f0a6e02d58551ee77d2b1397c36fa2e
MinhQuan0716 <minhquantrantrinh731@gmail.com>
2024-09-23 19:12:43 +0700

    Fix migration error

--- Infrastructure/Migrations/20240923083337_RenameServiceEntity.cs
+++ Infrastructure/Migrations/20240923083337_RenameServiceEntity.cs
@@ -32,11 +32,11 @@ namespace Infrastructure.Migrations
                  name: "CenterTankId",
                  table: "CenterServices");*/
 
-            migrationBuilder.InsertData(
+          /*  migrationBuilder.InsertData(
                 table: "Accounts",
                 columns: new[] { "Id", "ContactLink", "CreatedBy", "CreationDate", "DeletedBy", "DeletionDate", "Email", "Fullname", "IsDelete", "Location", "ModificationBy", "ModificationDate", "PasswordHash", "Phonenumber", "RoleId", "Username", "WorkingScheduleId" },
                 values: new object[] { new Guid("af29f656-eb16-410c-a7ab-3f7c56d88e32"), "", null, new DateTime(2024, 9, 23, 8, 33, 37, 335, DateTimeKind.Utc).AddTicks(2453), null, null, "admin@gmail.com", "Admin", false, "", null, null, "$2a$11$C/ogfzxlye2ZxB8GDUOny.vNgiGMhz1dFgfzeq1gBZwHiKebBGBLS", "", 1, "Admin", null });
-
+*/
             migrationBuilder.CreateIndex(
                 name: "IX_CenterServices_TankId",
                 table: "CenterServices",
